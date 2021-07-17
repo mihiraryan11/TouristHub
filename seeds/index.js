f292cf6b0ea9478357859d5930266b7738064313
Mihir <mihiraryan11@gmail.com>
2021-07-18 01:44:43 +0530

    Done basic styling(Navbar,Footers,inserted img)

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () =>{
     await TouristSpots.deleteMany({});
     for(let i=0;i<50;i++){
         const random1467 = Math.floor(Math.random()*1467);
+        const price= Math.floor(Math.random() * 300);
         const spot = new TouristSpots({
             location:`${cities[random1467].city}, ${cities[random1467].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur deleniti necessitatibus consequatur molestiae incidunt odit tempore ullam, aspernatur repudiandae suscipit sit minus quia quis velit! Fugiat quae recusandae esse omnis!',
+            price
         })
         await spot.save();
     }
