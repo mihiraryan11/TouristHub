64bf7a313e4834d96e514de97f2e829470056def
Mihir <mihiraryan11@gmail.com>
2021-07-19 14:24:13 +0530

    Done Error Handling and Validations

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express');
 const path=require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const {TouristSpotsSchema} =require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const TouristSpots = require('./models/TouristSpots');
 
 mongoose.connect('mongodb://localhost:27017/TouristHub',{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true
-})
-mongoose.set('useFindAndModify', false);
+    useUnifiedTopology:true,
+    useFindAndModify:false
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
@@ -27,47 +30,66 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req,res,next)=>{
+    const {error} = TouristSpotsSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400);
+    }else{
+        next();
+    }
+}
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
 
-app.get('/touristspots',async (req,res)=>{
+app.get('/touristspots',catchAsync(async (req,res)=>{
     const spots = await TouristSpots.find({});
     res.render('touristspots/index',{spots});
-})
+}))
 
 app.get('/touristspots/new',(req,res)=>{
     res.render('touristspots/new');
 })
 
-app.post('/touristspots',async (req,res)=>{
+app.post('/touristspots',validateCampground,catchAsync(async (req,res,next)=>{
     const spot = new TouristSpots(req.body.touristspot);
     await spot.save();
     res.redirect(`/touristspots/${spot._id}`);
-})
+}))
 
-app.get('/touristspots/:id',async (req,res)=>{
+app.get('/touristspots/:id',catchAsync(async (req,res)=>{
     const spot = await TouristSpots.findById(req.params.id)
     res.render('touristspots/show',{spot});
-})
+}))
 
-app.get('/touristspots/:id/edit',async (req,res)=>{
+app.get('/touristspots/:id/edit',catchAsync(async (req,res)=>{
     const spot = await TouristSpots.findById(req.params.id)
     res.render('touristspots/edit',{spot});
-})
+}))
 
-app.put('/touristspots/:id',async (req,res)=>{
+app.put('/touristspots/:id',validateCampground,catchAsync(async (req,res)=>{
     const {id}=req.params;
     const spot = await TouristSpots.findByIdAndUpdate(id,{...req.body.touristspot})
     res.redirect(`/touristspots/${spot._id}`);
-})
+}))
 
-app.delete('/touristspots/:id',async (req,res)=>{
+app.delete('/touristspots/:id',catchAsync(async (req,res)=>{
     const {id}=req.params;
     await TouristSpots.findByIdAndDelete(id)
     res.redirect(`/touristspots`);
+}))
+
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('Page Not Found',404))
 })
 
+app.use((err,req,res,next)=>{
+    const {statusCode = 500} = err;
+    if(!err.message)err.message = 'Something Went Wrong';
+    res.status(statusCode).render('error',{err});
+})
 
 app.listen(3000,()=>{
     console.log('Serving on 3000')
